8c1845f1fb6142cc6e3be93e02d4df84197e2487
AechGG <hazg@me.com>
2020-08-12 16:39:00 +0100

    feat: get data from coin api

--- Coin/CoinManager.swift
+++ Coin/CoinManager.swift
@@ -11,9 +11,29 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = ""
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getExchangeRate(_ currencyElem: Int) {
+        let urlString = "\(baseURL)/\(currencyArray[currencyElem])";
+        performRequest(with: urlString);
+    }
     
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default);
+            
+            var request = URLRequest(url: url);
+            request.setValue(apiKey, forHTTPHeaderField: "X-CoinAPI-Key");
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    print(error);
+                }
+                print(data);
+            };
+            
+            task.resume();
+        }
+    }
 }
